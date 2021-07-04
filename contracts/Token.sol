c6b884e7ca7cc3f6f92215ece6de3b7823406860
JangChorong <sweatpotato13@gmail.com>
2021-07-04 22:45:26 +0900

    feat: Add freeze

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,9 +8,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract Token is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pausable, ERC20Permit {
-    constructor() ERC20("TokenName", "TokenSymbol") ERC20Permit("TokenName") {
-        _mint(msg.sender, 1000000000 * 10 ** decimals());
+contract Token is
+    ERC20,
+    ERC20Burnable,
+    ERC20Snapshot,
+    Ownable,
+    Pausable,
+    ERC20Permit
+{
+    constructor()
+        ERC20("TokenName", "TokenSymbol")
+        ERC20Permit("TokenName")
+        Ownable()
+        Pausable()
+    {
+        _mint(msg.sender, 1000000000 * 10**decimals());
     }
 
     function snapshot() public onlyOwner {
@@ -29,11 +41,36 @@ contract Token is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pausable, ERC20P
         _mint(to, amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override(ERC20, ERC20Snapshot)
-    {
+    //freezable
+    event Frozen(address target);
+    event Unfrozen(address target);
+
+    mapping(address => bool) internal freezes;
+
+    modifier whenNotFrozen() {
+        require(!freezes[msg.sender], "Sender account is locked.");
+        _;
+    }
+
+    function freeze(address _target) public onlyOwner {
+        freezes[_target] = true;
+        emit Frozen(_target);
+    }
+
+    function unfreeze(address _target) public onlyOwner {
+        freezes[_target] = false;
+        emit Unfrozen(_target);
+    }
+
+    function isFrozen(address _target) public view returns (bool) {
+        return freezes[_target];
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Snapshot) whenNotPaused whenNotFrozen {
         super._beforeTokenTransfer(from, to, amount);
     }
 }
